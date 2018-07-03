8650a22861726351632fd0053f55de403585523a
wqhash <wqhash@163.com>
2018-07-03 22:18:45 +0800

    add master Sunday modify

--- spicyHotPot/underscore-1.8.2/sourceCodeLearning/underscoreLearning.js
+++ spicyHotPot/underscore-1.8.2/sourceCodeLearning/underscoreLearning.js
@@ -34,7 +34,7 @@
         return cb(value, context, Infinity);
     };
     //用于 _.each 中回调函数的[优化]判断处理 => 判断了有无context[this]对象,和 argCount
-    //返回一个 回调函数,用户传入的那个 
+    //返回一个 回调函数,用户传入的那个
     var optimizeCb = function (func, context, argCount) {
         // void 0 === "undefined"
         if (context === void 0) return func;
@@ -68,13 +68,13 @@
     };
     var createAssigner = function (keysFunc, undefinedOnly) {
         // function (obj) == _.extendOwn({}, attrs);
-        return function (obj) { 
+        return function (obj) {
             var length = arguments.length;
             if (length < 2 || obj == null) return obj;
             for (var index = 1; index < length; index++) {
                 var source = arguments[index],
                     keys = keysFunc(source),
-                    l = keys.length;    
+                    l = keys.length;
                 for (var i = 0; i < l; i++) {
                     var key = keys[i];
                     if (!undefinedOnly || obj[key] === void 0) obj[key] = source[key];
@@ -117,7 +117,7 @@
         //return 目前用不到 [可能只是这个方法以后的扩展吧]
         return obj;
     };
-    _.map = _.collect = function (obj, iteratee, context) {        
+    _.map = _.collect = function (obj, iteratee, context) {
         iteratee = cb(iteratee, context);
         var keys = !isArrayLike(obj) && _.keys(obj),
             length = (keys || obj).length,
@@ -132,8 +132,8 @@
     };
 
     function createReduce(dir) {
-        //iteratee (memo, value, index, list) 
-        function iterator(obj, iteratee, memo, keys, index, length) {    
+        //iteratee (memo, value, index, list)
+        function iterator(obj, iteratee, memo, keys, index, length) {
             for (; index >= 0 && index < length; index += dir) {
                 var currentKey = keys ? keys[index] : index;
                 //这里用户自定义的回调执行之后，赋值给memo 所以也就明确了在用户层 回调函数的 return 是必须的
@@ -249,7 +249,7 @@
             return !predicate.apply(this, arguments);
             //return !predicate(arguments)
 
-            // 返回 满足 predicate 条件的 
+            // 返回 满足 predicate 条件的
             //return !predicate.apply(this, arguments);
         };
     };
@@ -313,15 +313,15 @@
     _.keys = function (obj) {
         if (!_.isObject(obj)) return [];
         if (nativeKeys) return nativeKeys(obj);
-        var keys = []; 
+        var keys = [];
         // 手动处理  Object.keys  =>也是对数组的处理
         for (var key in obj) if (_.has(obj, key)) keys.push(key);
-        // Ahem, IE < 9. 
+        // Ahem, IE < 9.
         // wq: 暂时不考虑 低版本
         //if (hasEnumBug) collectNonEnumProps(obj, keys);
         return keys;
     };
-    // 这里传递的是是 实参 
+    // 这里传递的是是 实参
     // 注意：createAssigner函数内部有return 当别处调用 _.extendOwn(arg) == createAssigner(_.keys)(arg)
     _.extendOwn = _.assign = createAssigner(_.keys);
     _.isMatch = function (object, attrs) {
@@ -336,15 +336,15 @@
     };
 
     /**
-    *  判断对象中的属性是不是自身的（不包含prototype中的） 
+    *  判断对象中的属性是不是自身的（不包含prototype中的）
     */
     _.has = function (obj, key) {       //1261
-        // 用于判断 key 是不是 obj 的属性   
+        // 用于判断 key 是不是 obj 的属性
         // hasOwnProperty: 为了判断一个对象是否包含自定义属性而不是原型链上的属性[用hasOwnProperty很合理]
         // As: var obj = {"sname": "wq"} obj.prototype.sage = 11; "sage" in obj =>也会是true 而hashOwnProperty只会查找自身上的，不会找原型的
         return obj != null && hasOwnProperty.call(obj, key);
     };
-   
+
 
     //工具方法
     // -----------------
@@ -357,25 +357,25 @@
         return value;
     };
 
-    /**     
-    * 判断是不是一个函数 
-    * 返回Bool  
+    /**
+    * 判断是不是一个函数
+    * 返回Bool
     */
     if (typeof /./ != 'function' && typeof Int8Array != 'object') {
-        _.isFunction = function (obj) {  
-            return typeof obj == 'function' || false; 
+        _.isFunction = function (obj) {
+            return typeof obj == 'function' || false;
         };
     };
 
     /**
-    * 判断是不是一个对象 
+    * 判断是不是一个对象
     * 返回Bool
     */
     _.isObject = function (obj) {
        var type = typeof obj;
        return type === 'function' || type === 'object' && !!obj;
    };
-   
+
 
     _.matcher = _.matches = function (attrs) {
         attrs = _.extendOwn({}, attrs);
@@ -384,9 +384,9 @@
        };
    };
 
-    /** 
+    /**
     *  处理属性的方法
-    *  key是一个动态的值   _.property(key)(arr) 
+    *  key是一个动态的值   _.property(key)(arr)
     */
    _.property = function (key) {
        return function (obj) {
@@ -394,5 +394,5 @@
        };
    };
 
-  
+
 }.call(this));
