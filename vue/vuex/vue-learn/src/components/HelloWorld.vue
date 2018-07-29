06df40b558ebadc07fedd81a1acc45bcf287ba26
wqhash <wqhash@163.com>
2018-07-29 20:54:50 +0800

    vuex learning

---
+++ vue/vuex/vue-learn/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="">
+    <h2>vuex 学习</h2>
+    <div>
+    </div>  
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  methond(){},
+  computed: mapState({
+    count: state => state.count,
+    countAlias: 'count',
+    countPlusLocalState (state) {
+      return state.count + this.localCount
+    },
+  }),
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
