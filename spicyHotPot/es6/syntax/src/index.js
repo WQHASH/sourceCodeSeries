35235700cf5981c176eb7274a5908fd0f7ea0ef1
wangqihash <wangqihash@163.com>
2018-09-06 17:40:17 +0800

    Notes

--- spicyHotPot/es6/syntax/src/index.js
+++ spicyHotPot/es6/syntax/src/index.js
@@ -1,5 +1,6 @@
 // 文件引用 requier 按照文件引用的顺序打包  文件依赖
 // require('./style.less')
+// https://www.codercto.com/a/18867.html [***模块知识参考***]
 // wq: 这种采用require的方式进行模块管理 node.js就是采用这种管理的, 现在除了微信游戏这块可能还在使用这种导模块方式，其他都都采用es6的方式
 require('./styles/normalize');  //通用的ressa
 require('./styles/index');
