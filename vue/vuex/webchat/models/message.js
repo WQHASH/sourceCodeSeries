9bd057e64ba5f10dff63cd90e082ab8a52911fa9
wqhash <wqhash@163.com>
2018-09-28 22:54:23 +0800

    vueItem

--- vue/vuex/webchat/models/message.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose')
-var MessageSchema = require('../schemas/message')
-var Message = mongoose.model('Message',MessageSchema)
-
-module.exports = Message
--- vue/vuex/webchat/server_modules/mongodb.js
+++
@@ -1,7 +0,0 @@
-/*引入*/
-var mongoose = require('mongoose')
-// 用于异步回调
-mongoose.Promise = require('bluebird')
-global.db = mongoose.connect("mongodb://localhost:27017/vuechat")
-
-module.exports = mongoose;
