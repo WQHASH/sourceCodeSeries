c1d25dca9adaab55deb0c93712db66b8a6d8b099
wqhash <wqhash@163.com>
2019-04-06 11:13:58 +0800

    vue原理剖析

--- vue/vuex/imitable-vuex-test/src/vuex/modules/todo/mutation_type.js
+++
@@ -1,3 +0,0 @@
-export const ADD_TODO = 'ADD_TODO';
-export const DONE_TODO = 'DONE_TODO';
-export const SELECT_TYPE = 'SELECT_TYPE';
--- vue/vuex/vue-project/vue-toutiao/src/views/Layout/index.js
+++
@@ -1,2 +0,0 @@
-export { default as footerBar } from './footerBar' // 顶部bar
-export { default as headerBar } from './headerBar' // 顶部搜索bar
