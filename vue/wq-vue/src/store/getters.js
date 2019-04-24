caefe25032345344ba9d6cd58707fb4ad8c1ba10
wangqihash <wangqihash@163.com>
2019-04-24 17:21:33 +0800

    修改vue项目的文件结构

--- vue/wq-vue/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-    fileList: state => state.app.fileList,
-    problemList: state => state.app.problemList,
-    renameText:state => state.app.renameText,
-};
-
-export default getters
