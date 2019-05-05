423c24ae8f656dcaffabf564cfb4199f2f939111
wangqihash <wangqihash@163.com>
2019-05-05 17:36:02 +0800

    对于vuex中state不能直接修改理解

--- vue/vue-admin/wq-vue-element-admin/src/views/documentation/index.vue
+++ vue/vue-admin/wq-vue-element-admin/src/views/documentation/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>documentationdocumentation</div>
+  <div>
+      <input v-model="sidebar.opened">
+      {{sidebar.opened}}
+      <button  @click="change">按钮</button>
+  </div>
 </template>
 <script>
+import { mapGetters, mapActions } from "vuex";
 export default {
-  name: "documentation"
+  name: "documentation",
+  data(){return {} },
+  computed: {
+    ...mapGetters(["sidebar"])
+  },
+  methods:{
+    change(){
+      this.sidebar.opened = 11;
+    }
+  }
 };
 </script>
 
